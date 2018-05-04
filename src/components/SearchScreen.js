3c37c534925b5e78ea319ff30d751094b1c93430
Leh Aguiar <leticia.aad@gmail.com>
2018-05-04 10:27:42 -0300

    update searchScreen

--- src/components/SearchScreen.js
+++ src/components/SearchScreen.js
@@ -1,30 +1,46 @@
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View
-} from 'react-native';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { Actions } from 'react-native-router-flux';
+
+const windowSize = require('Dimensions').get('window')
+const deviceWidth = windowSize.width
+const deviceHeight = windowSize.height
 
 class SearchScreen extends Component{
-  
-//   openSearchModal() {
-//     RNGooglePlaces.openAutocompleteModal()
-//     .then((place) => {
-//                 console.log(place);
-//                 this.setState({
-//                   location: place
-//                 });
-//     })
-//     .catch(error => console.log(error.message));  // error  object
-//   }
 
   render(){
     return(
-      <View>
-        <Text>mapa</Text>
-      </View>
+      <GooglePlacesAutocomplete
+      placeholder='Pesquisar local'
+      minLength={2}
+      autoFocus={false}
+      onPress={(data) => {
+        Actions.push('details', { item : data});
+      }}
+      query={{
+        // available options: https://developers.google.com/places/web-service/autocomplete
+        key: 'AIzaSyDXjSZ-gsHxG-WqacY_ufb52WZAF9_jdpo',
+        language: 'pt-BR'
+      }}
+      
+      styles={{
+        textInputContainer: {
+          width: '100%'
+        },
+        description: {
+          fontWeight: 'bold'
+        },
+        predefinedPlacesDescription: {
+          color: '#1faadb'
+        },
+        listView: {
+          top: 40,
+          position: 'absolute',
+          height: deviceHeight,
+          width: deviceWidth
+        }
+      }}
+    />
     );
   }
 }
