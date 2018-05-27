2fe4421701c653a1831cb584e74d6581be278070
Leh Aguiar <leticia.aad@gmail.com>
2018-05-27 18:20:52 -0300

    badge assets

---
+++ src/assets/index.js
@@ -0,0 +1,10 @@
+const Badges = { 
+    key:  require('./key.jpg'),
+    p100: require('./100_p.png'),
+    p250: require('./250p.jpg'),
+    p500: require('./500p.jpg'),
+    p750: require('./750p.jpg'),
+    p1000: require('./1000p.jpg')
+}
+
+export default Badges;
